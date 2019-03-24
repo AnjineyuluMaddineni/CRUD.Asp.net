cf123a2e460e8a9276db1eae7a8924616dd5c477
AnjiMaddineni <Anjineyulu.Maddineni@gmail.com>
2019-03-24 11:34:01 +0000

    commit 05 Added SQL CRUD Queries.sql

---
+++ BusinessLayerClassLibrary/SQL CRUD Queries.sql
@@ -0,0 +1,10 @@
+﻿SELECT TOP (1000) [EmpID]
+      ,[EmpName]
+      ,[EmpAge]
+  FROM [chandu].[dbo].[EmployeeList]
+  insert into [dbo].[EmployeeList] (EmpID,EmpName,EmpAge)
+  values (2,'Teja',21)
+  select *from [dbo].[EmployeeList]
+  update [dbo].[EmployeeList]
+  set EmpID=1 where EmpID=2;
+  delete from [dbo].[EmployeeList] where EmpID=1
