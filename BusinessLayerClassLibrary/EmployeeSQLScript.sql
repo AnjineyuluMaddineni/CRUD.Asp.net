016142c8c39a9642289d6827ca0a6f384a1bfb84
AnjiMaddineni <Anjineyulu.Maddineni@gmail.com>
2019-03-24 11:26:58 +0000

    commit 04 Added SQL Employee Table Script

---
+++ BusinessLayerClassLibrary/EmployeeSQLScript.sql
@@ -0,0 +1,16 @@
+﻿USE [chandu]
+GO
+
+/****** Object:  Table [dbo].[EmployeeList]    Script Date: 3/24/2019 11:24:44 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[EmployeeList](
+	[EmpID] [nvarchar](50) NULL,
+	[EmpName] [varchar](50) NULL,
+	[EmpAge] [int] NULL
+) ON [PRIMARY]
+GO
